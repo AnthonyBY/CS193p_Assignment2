262384e549506902d7b6cfe595d5e28d842f275a
Anthony Marchenko <anthonymarchenko@gmail.com>
2013-12-14 14:24:43 +0300

    Add commit from Xcode 5

---
+++ Matchismo/PlayingCardViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardViewController.h
+//  Matchismo
+//
+//  Created by Anthony Marchenko on 12/14/13.
+//  Copyright (c) 2013 Anthony Marchenko. All rights reserved.
+//
+
+#import "CardGameViewController.h"
+
+@interface PlayingCardViewController : CardGameViewController
+
+@end
